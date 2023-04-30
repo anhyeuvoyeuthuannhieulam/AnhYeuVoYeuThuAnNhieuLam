4434e4c63d9f02986e2f989f8ab91a1c025ee018
anhyeuvoyeuthuannhieulam <132223269+anhyeuvoyeuthuannhieulam@users.noreply.github.com>
2023-05-01 01:18:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "anhyeuvoyeuthuan",
+    introTitle: 'Vợ yêu Thu An',
+    introDesc: `Anh yêu vợ yêu nhiều lắm, vợ cho anh xin lỗi nhé, anh biết lỗi của mình rồi <`,
+    btnIntro: '^^Yêu vợ nhiều lắm^^',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vợ cho anh xin lỗi nhaaaa <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Anh biết anh sai rồi vợ yêu ạ :((',
+    btnReply: 'Gửi cho vợ yêu <3',
+    reply: 'Yêu siuuuuuuu nhìu luônggggggggg <33333333',
+    mess: 'Yêu vợ nhiều 🥰. Yêu vợ nhiều lắmmm 😘😘',
+    messDesc: 'Nào anh ra với vợ yêu nhaaaa',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://api.onedrive.com/v1.0/drives/766D70ADC95502E6/items/766D70ADC95502E6!425/thumbnails/0/c516x688/content/With%20Her%2007.c516x688.png?prefer=noredirect&cb=2023-04-28T20%3A20%3A09.653Z' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
